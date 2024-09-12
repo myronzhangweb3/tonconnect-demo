3907185f1118599771aadca42d47bccda945b111
MyronZhang <zhangzhishun1024@gmail.com>
2024-09-12 17:38:36 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TonConnect Demo
+
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>TonConnect Demo</h1>
+    </header>
+    <TonConnectButton />
+  </div>
+</template>
+
+<script>
+import TonConnectButton from './components/TonConnectButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TonConnectButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+
+header {
+  background-color: #42b983;
+  padding: 20px;
+  color: white;
+}
+</style>
